1c05c2364424bc949c76f2e8e343fc31783779f8
Surbhi Harsh <surbhi.harsh@xebia.com>
2021-03-15 13:06:20 +0530

    adding swagger api documentation

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const app = express();
 const logger = require('./config/logger.config');
 const userRoutes = require('./routes/user.route');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const port = process.env.PORT || 3000;
 
 app.use(require('body-parser').urlencoded({extended: true}));
 app.use(require('body-parser').json())
 app.use(cors());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 userRoutes(app);
 
 app.listen(port, () => {
